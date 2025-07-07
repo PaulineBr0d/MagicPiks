4138306804c4692ffcb3dd7f7ea48ce3676f7f4c
PaulineBr0d <briaud.pauline@gmail.com>
2025-07-07 23:02:22 +0200

    mise àjour 070725

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # project_fodos
+https://www.figma.com/design/GC5v95K5CKXshqJNZi0CTG/projet_formation?node-id=3-1682&t=Nt0IzPanDyLM6y2a-0
---
+++ script.js
@@ -0,0 +1,27 @@
+const allBlocs = document.querySelectorAll('.bloc');
+
+
+allBlocs.forEach(bloc => {
+    bloc.addEventListener('click', (e) => {
+
+        // console.log(e.target);
+        e.target.classList.add('active');
+
+        for(let i = 0; i < allBlocs.length; i++ ){
+            if(allBlocs[i] !== e.target){
+                allBlocs[i].classList.remove('active');
+            }
+        }
+        
+
+    })
+})
+/*pour gérer le pointer events grâce à stop propagation:
+juste activer sur le button et non sur l'ens du bloc*/
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.stopPropagation();
+    })
+})
