99dff0aa714f6ffb7513c6353ec775068955b2fe
PaulineBr0d <briaud.pauline@gmail.com>
2025-08-24 23:27:55 +0200

    Update CORS configuration

--- server.js
+++ server.js
@@ -19,24 +19,8 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
-/*app.use(cors());*/
-const allowedOrigins = [
-  'http://localhost:3000', // local dev
-  'https://paulinebr0d.github.io/', 
-];
+app.use(cors());
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // autorise les requêtes sans origine (postman, curl, etc)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'L\'origine CORS n\'est pas autorisée.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true // si tu utilises les sessions / cookies
-}))
 app.use(express.json());
 
 // Import des routes
