65a94754934647124565af0203df153a2c048fb8
PaulineBr0d <briaud.pauline@gmail.com>
2025-08-25 21:39:40 +0200

    Ignore public/ (frontend externalisé)

--- public/js/checkAdmin.js
+++
@@ -1,7 +0,0 @@
-fetch('https://magicpiks-75022637d756.herokuapp.com/api/auth/check')
-  .then(res => res.json())
-  .then(data => {
-    if (!data.loggedIn) {
-      window.location.href = "login.html"; 
-    }
-  });
