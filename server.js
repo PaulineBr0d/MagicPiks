c1a6cd82708b954677ce219d4b0ee791e62b41b1
PaulineBr0d <briaud.pauline@gmail.com>
2025-09-08 15:39:48 +0200

    address

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 
 
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
+  "http://localhost:5501",
+  "http://127.0.0.1:5501",
   "https://paulinebr0d.github.io"
 ];
 // Configuration de la session dans Express
